dcc3b7eafbeb6493d05a6f996a2c028add84285b
ClerikT <c.terres@etud.univ-pau.fr>
2025-12-15 14:08:57 +0100

    Update gestionAdmin.php

--- code/espace_admin/gestionAdmin.php
+++ code/espace_admin/gestionAdmin.php
@@ -5,4 +5,4 @@ error_reporting(E_ALL);
 include_once './../parties_fixes/sidebar.php';
 ?>
 
-<!-- gestionAdmin.php -->
+<!-- gestionAdmin.php A NE PAS FAIRE CAR IL N'Y A PAS D'ADMIN, SEULEMENT DES GARAGES -->
