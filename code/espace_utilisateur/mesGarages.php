0376687a1e7ea606413408625e753276fd343f8d
Lucas Oustaloup <root@isa2-sae-2.univ-pau.fr>
2025-12-26 14:44:36 +0000

    normalisation de l'ensemble des pages non entamees dans espace_utilisateur pour eviter des problemes additionnels

--- code/espace_utilisateur/mesGarages.php
+++ code/espace_utilisateur/mesGarages.php
@@ -1,9 +1,22 @@
 <?php
+include_once './../parties_fixes/sidebar.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body class="d-flex">
+
+<?php
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+echo "oui";
 
-include_once './../parties_fixes/sidebar.php';
 ?>
 
 <!-- mesGarages.php -->
--- code/espace_utilisateur/prixEssence.php
+++ code/espace_utilisateur/prixEssence.php
@@ -1,17 +1,17 @@
+<?php 
+include_once './../parties_fixes/sidebar.php';
+?>
 <!DOCTYPE html>
-
 <html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
 </head>
-<body>
 
+<body class="d-flex">
 
-<?php 
-include_once './../parties_fixes/sidebar.php';
+<?php
 
 require './../vendor/autoload.php';
 use GuzzleHttp\Client;
@@ -27,7 +27,7 @@ echo "<br>".$response->getStatusCode();
 $body = $response->getBody()->getContents();
 $data = json_decode($body, true);
 echo $data["results"][0]["ville"];
-
+echo "vvvvvvvvvqernfqbrfkbqlrefblhqbrfhjlqbeflbqlbfvlqdsvfqvsfiuqfbiuqfiumqvbjdbvlqbvqberfliubqeurfvbulerbvudbfvbdfuvbljkbvjlkbiubiuerbfimqbeiumbjkvbkbvkmqbjbqjrfvbimqbf";
 
 ?>
     <br>
